570c9b968b59d307e4af3a0086c7a9613bbaf1f7
Kutalia <kotekutalia@gmail.com>
2025-08-15 12:46:19 +0400

    feat: auto-updates

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,13 +10,14 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![greet])
         .setup(|_app| {
             #[cfg(debug_assertions)]
             {
-                use tauri::Manager;
+                // use tauri::Manager;
                 
-                let main_window = app.get_webview_window("main").unwrap();
+                // let main_window = app.get_webview_window("main").unwrap();
                 // main_window.open_devtools();
                 // TODO: finish front-end implementing of https://github.com/Xinyu-Li-123/tauri-clickthrough-demo to allow drag/resizing captions window
                 // hook::start_global_mouse_stream(main_window);
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,37 @@ import './App.css'
 import Captions from './containers/Captions'
 import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow'
 
+import { check } from '@tauri-apps/plugin-updater';
+import { relaunch } from '@tauri-apps/plugin-process';
+
+const update = await check();
+if (update) {
+  console.log(
+    `found update ${update.version} from ${update.date} with notes ${update.body}`
+  );
+  let downloaded = 0;
+  let contentLength = 0;
+  // alternatively we could also call update.download() and update.install() separately
+  await update.downloadAndInstall((event) => {
+    switch (event.event) {
+      case 'Started':
+        contentLength = event.data.contentLength as number;
+        console.log(`started downloading ${event.data.contentLength} bytes`);
+        break;
+      case 'Progress':
+        downloaded += event.data.chunkLength;
+        console.log(`downloaded ${downloaded} from ${contentLength}`);
+        break;
+      case 'Finished':
+        console.log('download finished');
+        break;
+    }
+  });
+
+  console.log('update installed');
+  await relaunch();
+}
+
 const appWebview = getCurrentWebviewWindow()
 // Seems like it's not working
 appWebview.setIgnoreCursorEvents(true)
